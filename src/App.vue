c47edd8f189bd32b331061a52ceec9cdb752e19c
Miguel Chavez <chavezmendoza727@gmail.com>
2024-04-22 13:38:27 -0600

    🌟Nested Components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script setup>
-import {ref} from 'vue';
-import planItem from './components/plan-item.vue'   //Estamos importando la identidad
-const plans = ref(["El soltero", "El adicto", "El viajero", "El colombiano"]);
+// import {ref} from 'vue';
+// import planItem from './components/plan-picker-item.vue';
+// const plans = ref(["El soltero","El adicto","El viajero","El colombiano"]);
+import planPicker from './components/plan-picker.vue';  //(se esta importando) Siempre especificar bien la ruta
 </script>
-
+ 
 <template>
-  <header>
-    <div>
-      <span id="logo">Liquid Gold Box</span>
-      <img src="./assets/logo.png" alt="logo">
-    </div>
-  </header>
-
-  <div class="content">
-    <h1 class="title">Coffee Plans</h1>
-
-    <h2 class="subtitle">
+<header>
+<div>
+<span id="logo">Liquid Gold Box</span>
+<img src="./assets/logo.png" alt="logo">
+</div>
+</header>
+<div class="content">
+<h1 class="title">Coffee Plans</h1>
+<h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
-    </h2>
+</h2>
+ 
+    <!-- <div class="plans">
+<plan-item v-for="plan in plans"
+      :name="plan" 
+      v-bind:key="plan"/>
+</div> -->
 
-    <div class="plans">
-      <plan-item v-for="plan in plans" :name="plan" v-bind:key="plan"/>    
-    </div>
-  </div>
+<plan-picker/>
+</div>
 </template>
-
+ 
 <style scoped>
 header {
   line-height: 1.5;
---
+++ src/components/plan-picker-item.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="plan">
+  <div class="description">
+  <span class="title">  
+              {{ name }} 
+  </span>
+  </div>
+  </div>
+  </template>
+  <script setup>
+  //Macro es una funcion especialidad en vue que me permite crear defineProps (que ya las importa), no se tiene que intregrar porque Vue ya lo hace, para agregar  nombre propiedades siempre usar camelcasing
+  defineProps({
+    name: {type: String, 
+      required: true,}
+  });
+  //Esquema de propiedades de manera mas avanzada (objeto), las propiedades van dentro de las llaves {} 
+  //con el objetivo de proveer un objeto, no un arreglo
+  </script>
