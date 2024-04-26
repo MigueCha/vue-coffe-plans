5e4edcad9cd43e6be273962a221d68a982c779a0
Miguel Chavez <chavezmendoza727@gmail.com>
2024-04-26 11:58:37 -0600

    ☕Coffe-Plans Done

--- src/components/plan-picker-item.vue
+++ src/components/plan-picker-item.vue
@@ -1,41 +1,51 @@
 <template>
   <div
         @click="select"
-        :class="{'selected-plan':selected}" 
+        :class="{'selected-plan': isSelected }" 
         class="plan">
        <div class="description">
          <span class="title">  
-           {{ name }} {{ selected ? "👍" :"" }}
+           {{ name }}
          </span>
        </div>
-     </div>
+     </div> 
 </template>
 
 
 <script setup>
-import {ref} from 'vue';
+import { computed } from 'vue';
 
+//Crea una propiedad selectedPlan
 //Creando Eventos
 const emit = defineEmits(['select']);
 
 //Macro es una funcion especialidad en vue que me permite crear defineProps (que ya las importa), no se tiene que intregrar porque Vue ya lo hace, para agregar  nombre propiedades siempre usar camelcasing
 const props = defineProps({
- name: {type: String,
-   required: true,}
+ name: {
+  type: String,
+   required: true,},
+
+   selectedPlan: {
+    type: String
+   },
 });
 
 //Definir una variable, permitiendo a cada item saber si a sido seleccionada o no
-const selected = ref(false);
 
 //metodo para cambiar a true
 const select = ()=> {
- selected.value = true
  //Detonando el Evento
  emit('select', props.name);
 }
 
 //Esquema de propiedades de manera mas avanzada (objeto), las propiedades van dentro de las llaves {}
 //con el objetivo de proveer un objeto, no un arreglo
+
+//Creando propiedad computada
+const isSelected = computed(() => {
+  return props.name === props.selectedPlan;
+})
+
 </script>
 
 <style scoped>
